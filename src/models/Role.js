0840f95c3092c1f4fd790784597fa5ff9a3b4602
Cristian Alderete <catastones@gmail.com>
2023-10-05 11:17:47 -0300

    model role

---
+++ src/models/Role.js
@@ -0,0 +1,11 @@
+import { Schema, model } from "mongoose";
+
+export const RolesSistem = ['user','admin','client'];
+const RoleSchema = new Schema({
+    name: String
+
+},{
+    versionKey:false
+});
+
+export default model('Role',RoleSchema);
