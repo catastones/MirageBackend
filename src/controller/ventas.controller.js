aa0d65744b631b0f4ba0dd0a1be92b8820727dba
Cristian Alderete <catastones@gmail.com>
2023-10-09 18:00:01 -0300

    router ventas

--- src/controller/ventas.controller.js
+++ src/controller/ventas.controller.js
@@ -1,8 +1,11 @@
 import ventas from '../models/Venta'
 
 export const newVenta = async (req, res )=>{
-    
+
 }
 export const listVenta = async (req, res )=>{}
-export const getVenta = async (req, res )=>{}
+export const getVentaById = async (req, res )=>{}
+export const getVentaByBuyer = async (req, res )=>{}
+export const getVentaByEntreFechas = async (req, res )=>{}
 export const deleteVenta = async (req, res )=>{}
+export const updateVenta = async (req, res )=>{}
---
+++ src/routes/ventas.router.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+const router = Router();
+
+import * as ventasCtrl from '../controller/ventas.controller'
+
+
+ router.post('/',ventasCtrl.newVenta);
+ router.get('/', ventasCtrl.listVenta);
+ router.get('/:ventaId', ventasCtrl.getVentaById);
+ router.get('/bybuyer/:buyerId', ventasCtrl.getVentaByBuyer);
+router.get('/entrefechas/:from&:to', ventasCtrl.getVentaByBuyer);
+ router.delete('/:ventaId',ventasCtrl.deleteVenta);
+ router.put('/',ventasCtrl.updateVenta);
+
+export default router;
