61e2d99022eb04b59edf814fc70e4cf69c338736
Cristian Alderete <catastones@gmail.com>
2023-10-09 17:37:29 -0300

    se crea ventas controller

---
+++ src/controller/ventas.controller.js
@@ -0,0 +1,8 @@
+import ventas from '../models/Venta'
+
+export const newVenta = async (req, res )=>{
+    
+}
+export const listVenta = async (req, res )=>{}
+export const getVenta = async (req, res )=>{}
+export const deleteVenta = async (req, res )=>{}
