3da9e7b7e01f47270915fe7ed60e294c8787b824
Cristian Alderete <catastones@gmail.com>
2023-10-09 18:01:53 -0300

    add route ventas to app

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ import {createRoles, createCategoria} from './libs/initialSetup'
 import userRoutes from './routes/user.routes'
 import categoriaRoutes from './routes/categorias.router'
 import productosRoutes from './routes/producto.router'
+import ventasRoutes from './routes/ventas.router'
+
 //
 const app = express();
 app.use(cors({
@@ -30,5 +32,6 @@ app.get('/', (req,res)=>{
  app.use('/user',userRoutes);
  app.use('/categorias',categoriaRoutes)
   app.use('/productos',productosRoutes);
+  app.use('/ventas',ventasRoutes);
 
 export default app;
